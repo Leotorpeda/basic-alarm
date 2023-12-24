7dc9d88bad3df6f1995b12dce49c31e66f62ff06
Leonard Pallison <154711537+Leotorpeda@users.noreply.github.com>
2023-12-24 02:04:51 +0200

    Create simple notification.py

---
+++ simple notification.py
@@ -0,0 +1,15 @@
+simple notification
+
+CLIENT_VERSION = "29.0.0+snapshot"
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+
+from .quantity import parse_quantity
+
+    def test_job_apis(self):
+        client = api_client.ApiClient(configuration=self.config)
+        api = batch_v1_api.BatchV1Api(client)
+
+        create_job_resp = api.create_namespaced_job(
+            body=job_manifest, namespace='default')
+        self.assertEqual(name, create_job_resp.metadata.name)
